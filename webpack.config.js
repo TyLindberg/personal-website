de94956c6bb0603ef1f93b762b639ab05c9bc86e
Tyler Lindberg <tylindberg@hotmail.com>
2021-06-27 12:11:30 -0700

    Change to function syntax for webpack config
    Also set clean to true when mode is production

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,33 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: './src/index.ts',
-  devtool: 'source-map',
-  mode: 'development',
-  module: {
-    rules: [
-      { test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ },
-      { test: /\.html$/, use: 'html-loader' },
-      { test: /\.css$/i, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
-      { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource' },
+module.exports = (_, options) => {
+  const mode = options.mode || 'development';
+  return {
+    entry: './src/index.ts',
+    devtool: 'source-map',
+    mode,
+    module: {
+      rules: [
+        { test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ },
+        { test: /\.html$/, use: 'html-loader' },
+        { test: /\.css$/i, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
+        { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource' },
+      ],
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: mode === 'production' ? true : false,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Tyler Lindberg',
+        template: 'src/index.html',
+        hash: true,
+      }),
+      new MiniCssExtractPlugin(),
     ],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Tyler Lindberg',
-      template: 'src/index.html',
-      hash: true,
-    }),
-    new MiniCssExtractPlugin(),
-  ],
-  resolve: { extensions: ['.ts', '.js'] },
+    resolve: { extensions: ['.ts', '.js'] },
+  };
 };
