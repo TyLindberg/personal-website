31de9c57ef7bc4b3bb56faff4943c4fb78f5446a
Tyler Lindberg <tylindberg@hotmail.com>
2021-06-27 01:21:37 -0700

    Initial page layout

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-interface Test {
-  a: string;
-  b: number;
-}
-
-const test: Test = { a: 'test', b: 1 };
-
-console.log(test);
+import './styles.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.ts',
   devtool: 'source-map',
   mode: 'development',
   module: {
-    rules: [{ test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ }],
+    rules: [
+      { test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ },
+      { test: /\.html$/, use: 'html-loader' },
+      { test: /\.css$/i, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
+      { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource' },
+    ],
   },
   output: {
     filename: 'bundle.js',
@@ -16,7 +22,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Tyler Lindberg',
+      template: 'src/index.html',
     }),
+    new MiniCssExtractPlugin(),
   ],
   resolve: { extensions: ['.ts', '.js'] },
 };
