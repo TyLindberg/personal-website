f6b9ba2304a2a60a2fc1a42ab4165c97469fb855
Tyler Lindberg <tylindberg@hotmail.com>
2023-11-26 01:23:42 -0800

    Change from JS resizing for canvas to flexbox

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,11 @@ import { WebGLContext } from './webgl/context';
 import { WebGLLoader } from './webgl/loader';
 import { PerspectiveCamera } from './webgl/camera';
 import { WebGLCameraController } from './webgl/cameraController';
-import { resizeCanvas } from './utils/canvasResizer';
 
 const canvas = document.querySelector('canvas');
 if (!canvas || !(canvas instanceof HTMLCanvasElement)) {
   throw new Error('Failed to find HTML canvas!');
 }
-
-window.addEventListener('resize', () => resizeCanvas(canvas));
-resizeCanvas(canvas);
 
 const camera = new PerspectiveCamera([0, 0.1, 0]);
 const cameraController = new WebGLCameraController(camera, {
